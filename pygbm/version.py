218c45dd11f3e99de3643cbce53ee1906f120b1f
AndreaSainz <as3628@cam.ac.uk>
2024-11-05 12:25:10 +0000

    Agregando solo la carpeta Problem2_C1

--- pygbm/version.py
+++ pygbm/version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.0b1.dev2+gc4a0fb7.d20241101'
-__version_tuple__ = version_tuple = (0, 0, 0, 'dev2', 'gc4a0fb7.d20241101')
+__version__ = version = '0.0.0b1.dev0+gbbb1feb.d20241105'
+__version_tuple__ = version_tuple = (0, 0, 0, 'dev0', 'gbbb1feb.d20241105')
